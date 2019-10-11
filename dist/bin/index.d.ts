6004a9cb9f2f1bce73f2ec817a84a3ab9a03ace7
Anthanh <antai.pt@gmail.com>
2019-10-11 11:17:27 +0200

    add dummy entry point

---
+++ dist/bin/index.d.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+declare const profile: any;
+declare const cucumber: any;
+declare const cli: any;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as book } from './lib/book';
-export { default as BasePO } from './lib/book/base.po';
-export { testControllerHolder } from './lib/test-controller-holder';

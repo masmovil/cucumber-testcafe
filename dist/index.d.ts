bb102bc79cb5e75340f028f33fbc2e218d16bd41
Anthanh <antai.pt@gmail.com>
2019-10-11 11:15:27 +0200

    add dummy entry point

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { default as book } from './lib/book';
+export { default as BasePO } from './lib/book/base.po';
+export { testControllerHolder } from './lib/test-controller-holder';
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var book_1 = require("./lib/book");
+exports.book = book_1.default;
+var base_po_1 = require("./lib/book/base.po");
+exports.BasePO = base_po_1.default;
+var test_controller_holder_1 = require("./lib/test-controller-holder");
+exports.testControllerHolder = test_controller_holder_1.testControllerHolder;
+//# sourceMappingURL=index.js.map

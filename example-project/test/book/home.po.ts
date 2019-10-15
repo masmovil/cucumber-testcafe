763fe2e3759433ff9aca1de9444100823f237d7c
Anthanh <antai.pt@gmail.com>
2019-10-15 16:32:09 +0200

    minor change

--- example-project/test/book/home.po.ts
+++ example-project/test/book/home.po.ts
@@ -2,7 +2,7 @@ import { BasePO } from 'cucumber-testcafe'
 import { testController } from 'cucumber-testcafe/dist/lib/world'
 
 export default class HomePO extends BasePO {
-  static pageName: 'home'
+  static pageName = 'home'
 
   async assertCookies() {
     const cookies = await this.select('div[class*="cookies close"]')
@@ -22,34 +22,28 @@ export default class HomePO extends BasePO {
         return testController.click(
           this.selectByDataHook('link-home-menu-convergent')
         )
-        break
 
       case 'tarifas movil':
         return testController.click(
           this.selectByDataHook('link-home-menu-mobileonly')
         )
-        break
 
       case 'móviles y más':
         return testController.click(
           this.selectByDataHook('link-home-menu-terminals')
         )
-        break
 
       case 'sólo fibra':
         return testController.click(
           this.selectByDataHook('link-home-menu-fixed')
         )
-        break
 
       case 'tv a lo yoigo':
         return testController.click(this.selectByDataHook('link-home-menu-tv'))
-        break
       default:
         return testController
           .expect(true)
           .eql(false, `Case ${menuOption} not recognized`)
-        break
     }
   }
 

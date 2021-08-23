96c052fd92d03d8881614430aed5eff4eb31f123
jpatel01-tc <jpatel01@tucows.com>
2021-08-23 19:43:29 -0300

    report portal

--- src/lib/world.ts
+++ src/lib/world.ts
@@ -2,34 +2,56 @@ import { base64Sync } from 'base64-img'
 import { setWorldConstructor } from 'cucumber'
 import { testControllerHolder } from './test-controller-holder'
 
+let {RPWorld}  = require('@reportportal/agent-js-cucumber')
 export interface TestControllerWithTestRun extends TestController {
   executionChain?: any
   testRun?: any
 }
+let browser: TestControllerWithTestRun = null
+class CustomWorld extends RPWorld{
 
-let testController: TestControllerWithTestRun = null
+// attach: any
+// params: any
 
-export function CustomWorld({ attach, parameters }) {
-  this.waitForTestController = testControllerHolder
-    .get()
-    .then((tc: TestController) => {
-      testController = tc
-      return tc
-    })
+waitForTestController = testControllerHolder
+.get()
+.then((tc: TestController) => {
+  browser = tc
+  return tc
+})
 
-  this.attach = attach
-
-  this.parameters = parameters
-
-  this.attachScreenshotToReport = pathToScreenshot => {
-    const imgInBase64 = base64Sync(pathToScreenshot)
-    const imageConvertForCuc = imgInBase64.substring(
-      imgInBase64.indexOf(',') + 1
-    )
-    return attach(imageConvertForCuc, 'image/png')
-  }
+attachScreenshotToReport = pathToScreenshot => {
+  const imgInBase64 = base64Sync(pathToScreenshot)
+  const imageConvertForCuc = imgInBase64.substring(
+    imgInBase64.indexOf(',') + 1
+  )
+  return this.attach(imageConvertForCuc, 'image/png')
 }
+}
+
+
+
+// export function CustomWorld2({ attach, parameters }) {
+//   this.waitForTestController = testControllerHolder
+//     .get()
+//     .then((tc: TestController) => {
+//       testController = tc
+//       return tc
+//     })
+
+//   this.attach = attach
+
+//   this.parameters = parameters
+
+//   this.attachScreenshotToReport = pathToScreenshot => {
+//     const imgInBase64 = base64Sync(pathToScreenshot)
+//     const imageConvertForCuc = imgInBase64.substring(
+//       imgInBase64.indexOf(',') + 1
+//     )
+//     return attach(imageConvertForCuc, 'image/png')
+//   }
+// }
 
 setWorldConstructor(CustomWorld)
 
-export { testController }
+export { browser }

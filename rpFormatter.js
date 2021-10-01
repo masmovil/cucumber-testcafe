6c0e3cc68826734c2c3bed33b2a7877920360ca4
Jateen Patel <jpatel01@tucows.com>
2021-10-01 19:50:35 -0300

    minor updates

--- rpFormatter.js
+++ rpFormatter.js
@@ -1,4 +1,3 @@
 const { createRPFormatterClass } = require('@reportportal/agent-js-cucumber')
-const config = require('./rpConfig.json')
-
-module.exports = createRPFormatterClass(config)
+const config = require('../../configs/reportPortalConfig.js')
+module.exports = createRPFormatterClass(config.default)
--- src/lib/profile-loader.ts
+++ src/lib/profile-loader.ts
@@ -49,7 +49,6 @@ function cucumberProfileArgs(profile) {
   mergedProfile.timeout = process.env.CUCUMBER_TIMEOUT || mergedProfile.timeout
   mergedProfile.baseURL = process.env.CUCUMBER_BASEURL || mergedProfile.baseURL
   mergedProfile.tags = process.env.CUCUMBER_TAGS || mergedProfile.tags
-
   console.info('computed profile: ', mergedProfile)
 
   const args = [
@@ -63,14 +62,9 @@ function cucumberProfileArgs(profile) {
     `--tags`,
     `${mergedProfile.tags}`,
 
-    '--format',
-    `json:${mergedProfile.reports}/report.json`,
-
-    '--format',
-     'local_modules/cucumber-testcafe/rpFormatter.js',
-
     `--parallel`,
     `${mergedProfile.parallel}`,
+    ...mergedProfile.formatters,
     `--retry`,
     `${mergedProfile.retry}`
   ]

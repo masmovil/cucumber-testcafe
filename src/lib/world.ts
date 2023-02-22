271a93b33ce25a2efb8090d9c051c92be5ad6987
Jateen Patel <jpatel01@tucows.com>
2023-02-22 13:35:57 -0500

    moving forward with AllureJS remove ReportPortal

--- src/lib/world.ts
+++ src/lib/world.ts
@@ -1,6 +1,7 @@
 import { base64Sync } from 'base64-img'
 import { setWorldConstructor } from '@cucumber/cucumber'
 import { testControllerHolder } from './test-controller-holder'
+import { CucumberAllureWorld } from "allure-cucumberjs"
 
 let { RPWorld } = require('@reportportal/agent-js-cucumber')
 export interface TestControllerWithTestRun extends TestController {
@@ -8,13 +9,13 @@ export interface TestControllerWithTestRun extends TestController {
   testRun?: any
 }
 let browser: TestControllerWithTestRun = null
-class CustomWorld extends RPWorld {
+class CustomWorld extends CucumberAllureWorld {
   attach: any
   parameters: any
   currPage: any 
   sharedData: any
   constructor(attach, parameters) {
-    super(attach,parameters)
+    super(attach)
     this.currPage = null 
     this.sharedData= {}
   }
@@ -28,10 +29,10 @@ class CustomWorld extends RPWorld {
 
   attachScreenshotToReport = (pathToScreenshot) => {
     const imgInBase64 = base64Sync(pathToScreenshot)
-    const imageConvertForCuc = imgInBase64.substring(
-      imgInBase64.indexOf(',') + 1
-    )
-    return this.attach(imageConvertForCuc, 'image/png')
+     const imageConvertForCuc = imgInBase64.substring(
+       imgInBase64.indexOf(',') + 1
+     )
+    return this.attach(pathToScreenshot, 'image/png')
   }
 }
 
